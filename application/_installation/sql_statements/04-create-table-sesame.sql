d0ca0f4a0b1aa0928493fdacb58d650f9d7ae49d
thierryve <thierryve@hotmail.com>
2014-08-19 21:24:23 +0200

    Sql script to create sesame table in MySql database. This table will be used to store sesame random code and after login add the logged in user id.

---
+++ application/_installation/sql_statements/04-create-table-sesame.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `sesame` (
+  `sesame_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'auto incrementing sesame_id of each generated code, unique index',
+  `random_code` int(8) NOT NULL COMMENT 'Random generated sesame login code ',
+  `logged_in_user_id` int(11) unsigned NULL COMMENT 'If null randomcode is not used to login'
+) COMMENT='Sesame table to store generated login keys' ENGINE='InnoDB' COLLATE 'utf8_general_ci';
