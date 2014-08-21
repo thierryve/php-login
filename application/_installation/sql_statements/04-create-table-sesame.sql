3aa0408adf7976bcc917f170447beb899eaf0385
thierryve <thierryve@hotmail.com>
2014-08-21 23:14:46 +0200

    MySql Cleanup event that automatically removes all keys that are older then 1 hour

--- application/_installation/sql_statements/04-create-table-sesame.sql
+++ application/_installation/sql_statements/04-create-table-sesame.sql
@@ -1,5 +1,29 @@
+DELIMITER |
+
+/**
+Create sesame table
+*/
 CREATE TABLE `sesame` (
   `sesame_id` int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'auto incrementing sesame_id of each generated code, unique index',
   `random_code` int(8) NOT NULL COMMENT 'Random generated sesame login code ',
-  `logged_in_user_id` int(11) unsigned NULL COMMENT 'If null randomcode is not used to login'
-) COMMENT='Sesame table to store generated login keys' ENGINE='InnoDB' COLLATE 'utf8_general_ci';
+  `logged_in_user_id` int(11) unsigned NULL COMMENT 'If null randomcode is not used to login',
+  `created_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create timestamp used for cleanup purposes'
+) COMMENT='Sesame table to store generated login keys' ENGINE='InnoDB' COLLATE 'utf8_general_ci'|
+
+/**
+turn event_scheduler on, Note that you need root access to do this
+Create event, event will be triggered every 1 hour and will delete all record that are older then 1 hour
+for more info, see http://www.sitepoint.com/working-with-mysql-events/
+*/
+SET GLOBAL event_scheduler = ON|
+
+DROP EVENT IF EXISTS sesame_cleanup|
+
+CREATE EVENT sesame_cleanup
+    ON SCHEDULE EVERY 1 HOUR
+    DO
+      BEGIN
+        DELETE FROM sesame WHERE created_timestamp > DATE_SUB(CURDATE(), INTERVAL 1 HOUR);
+      END |
+
+DELIMITER ;
