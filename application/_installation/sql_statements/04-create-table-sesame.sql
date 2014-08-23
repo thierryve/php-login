45fdead5eb964ed6d83d13eafc0d74c3bdabbb18
thierryve <thierryve@hotmail.com>
2014-08-23 09:21:13 +0200

    random_code must be unique, this is forced in the database

--- application/_installation/sql_statements/04-create-table-sesame.sql
+++ application/_installation/sql_statements/04-create-table-sesame.sql
@@ -5,7 +5,7 @@ Create sesame table
 */
 CREATE TABLE `sesame` (
   `sesame_id` int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'auto incrementing sesame_id of each generated code, unique index',
-  `random_code` int(8) NOT NULL COMMENT 'Random generated sesame login code ',
+  `random_code` int(8) NOT NULL UNIQUE COMMENT 'Random generated sesame login code, must be unique',
   `logged_in_user_id` int(11) unsigned NULL COMMENT 'If null randomcode is not used to login',
   `created_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create timestamp used for cleanup purposes'
 ) COMMENT='Sesame table to store generated login keys' ENGINE='InnoDB' COLLATE 'utf8_unicode_ci'|
